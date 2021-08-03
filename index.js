1c9d3cde1be58407330a97f52e4d61cad5f55e17
MaffPerez <plzamfr@gmail.com>
2021-08-03 11:23:18 -0500

    valida si la ruta es absoluta

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 module.exports = () => {
   // ...
 };
+
+let path = require ('path');
+
+let ruta = process.argv[2];
+
+function isAbsolute (ruta){
+if(path.isAbsolute(ruta)){
+console.log("si es absoluta");
+return ruta
+}else{
+  console.log("no es absoluta");
+  return path.resolve(ruta);
+}
+}
+isAbsolute(ruta);
+
