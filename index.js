15a64ca21dde9bf0e457bce5c4032b9cfe6b0a57
MaffPerez <plzamfr@gmail.com>
2021-08-03 19:44:51 -0500

    lee si es un archivo o directorio

--- index.js
+++ index.js
@@ -4,17 +4,38 @@ module.exports = () => {
 
 const path = require ('path');
 const ruta = process.argv[2];
+let fs = require('fs');
 
-function isAbsolute (ruta){
+/*-------------------------Funcion que comprueba si la ruta es absoluta--------------------------*/
+function isAbsolute (){
 if(path.isAbsolute(ruta)){
-console.log("si es absoluta");
+console.log("es una ruta absoluta");
 return ruta
 }else{
-  console.log("no es absoluta");
+  console.log("no es una ruta absoluta");
   return path.resolve(ruta);
+}}
+
+function isArchive () {
+  const stats = fs.statSync(ruta);
+if (stats.isFile() === true) {
+  console.log("es un archivo");
+}else {
+  console.log("no es un archivo")
+}}
+
+function readElemnts () {
+fs.readFile(ruta, "utf8", function(err, data){
+  if(err){
+    console.log("es un directorio");
+  }else{
+    console.log("el archivo contiene"+ data);
+  }
+});
 }
-}
+
+
 isAbsolute(ruta);
-
-
+isArchive(ruta);
+readElemnts(ruta);
 
