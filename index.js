b481856e9f6fb358cdc6ec7c91f97e2f0ef44137
MaffPerez <plzamfr@gmail.com>
2021-08-06 16:49:38 -0500

    fx readDir lee lo que contiene el directorio

--- index.js
+++ index.js
@@ -6,76 +6,85 @@ const path = require ('path');
 const userPath = process.argv[2];
 const fs = require('fs');
 
-/*---------------------Funcion que comprueba si existe----------------------*/
-function checkExists (ruta){
-if(fs.existsSync(ruta)){
+/*---------------------1.Funcion que comprueba si existe----------------------*/
+function checkExists (route){
+if(fs.existsSync(route)){
   console.log("si existe");
 }else{
   console.log("no existe");
 }}
 
-/*---------------------Funcion que comprueba si la ruta es absoluta----------------------*/
-function convertAbsolutePath(ruta){
-  if(path.isAbsolute(ruta)){
-  console.log("es una ruta absoluta");
-  return ruta
-  }else{
-    console.log("no es una ruta absoluta");
-    return path.resolve(ruta);
-}}
+/*---------------------2.Funcion que comprueba si la ruta es absoluta----------------------*/
+const convertToAbsolute = (route) => path.resolve(route)
 
-/*---------------------Funcion que comprueba si es directorio----------------------*/
-  function enterDirectory (ruta) {
-    const stats = fs.statSync(ruta);
-  if (stats.isDirectory(ruta)) {
-    console.log("La ruta es un directorio");
-  }else {
-    console.log("no es un directorio");
-}}
 
-function enterFile(ruta) {
-  const stats = fs.statSync(ruta);
-  if (stats.isFile(ruta)) {
-    console.log("La ruta es un archivo");
-  }else {
-    console.log("no es un archivo");
-}}
+/*-------------------3.Funcion que verifica si es directorio o archivo--------------------*/
+const itsDirectory = (route) => fs.statSync(route).isDirectory()
 
-/*---------------------Funcion que lee el directorio----------------------*/
-function readElemnts (ruta) {
-fs.readdir(ruta, "utf8", function(err, data){
-  if(err){
-    console.log("no es un directorio");
-  }else{
-    console.log("el directorio contiene "+ data);
-  }
-})
+const itsFile = (route) => fs.statSync(route).isFile()
+
+
+/*---------------------4.Funcion que comprueba si tiene archvos md----------------------*/
+function mdExtension(route) {
+  return path.extname(route)
 }
 
+/*---------------------5.Funcion que lee el directorio----------------------*/
+
+function readDir (ruta) {
+  fs.readdir(ruta, "utf8", function(err, data){
+    if(err){
+      console.log("no es un directorio");
+    }else{
+      console.log("el directorio contiene "+ data);
+    }
+  })
+  }
+
 checkExists(userPath);
-convertAbsolutePath(userPath);
-enterDirectory(userPath);
-enterFile(userPath);
-readElemnts(userPath);
+console.log(convertToAbsolute(userPath));
+console.log("es directorio:",itsDirectory(userPath));
+console.log("es archivo:",itsFile(userPath));
+console.log(mdExtension(userPath));
+readDir(userPath);
 
-// function readElemnts () {
-// fs.readdir(ruta, "utf8", function(err, data){
-//   if(err){
-//     console.log("no es un directorio");
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function convertToAbsolutePath(ruta){
+//   if(path.isAbsolute(ruta)){
+//   console.log("es una ruta absoluta");
+//   return ruta
 //   }else{
-//     console.log("el archivo contiene "+ data);
-//   }
-// })
-// }
+//     console.log("no es una ruta absoluta");
+//     return path.resolve(ruta);
+// }}
 
+//   function enterDirectory (ruta) {
+//     const stats = fs.statSync(ruta);
+//   if (stats.isDirectory(ruta)) {
+//     console.log("La ruta es un directorio");
+//   }else {
+//     console.log("no es un directorio");
+// }}
 
-
-
-// isDirectory (ruta);
-// // isArchive(ruta);
-// readElemnts(ruta);
-
-
+// function enterFile(ruta) {
+//   const stats = fs.statSync(ruta);
+//   if (stats.isFile(ruta)) {
+//     console.log("La ruta es un archivo");
+//   }else {
+//     console.log("no es un archivo");
+// }}
 
 
 
